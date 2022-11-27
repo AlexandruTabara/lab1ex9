9f2013dc6634edf2b56c92b2538ee8725100f40d
Alexandru Tabara <alexandru.tab1@gmail.com>
2022-11-27 15:59:25 +0200

    Add project files.

---
+++ lab1ex9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab1ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ex 9
+                Scrieti un program care interschimba valoarea a doua variabile intregi */
+            int a = 5;
+            int b = 10;
+            Console.WriteLine("Inainte de interschimbare a= " + a + " b= " + b);
+
+            a= a+b;//a este 15
+            b= a-b;//b este 5
+            a= a-b;//a este 10 acum 
+
+            Console.Write("Dupa interschimbare a= " + a + " b= " + b);
+
+        }
+    }
+}
